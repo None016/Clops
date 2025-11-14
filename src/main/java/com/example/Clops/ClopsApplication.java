9096c6247df349dec1b954532210d69c08a81849
none016 <101492110+None016@users.noreply.github.com>
2025-11-14 21:50:38 +0500

    инициализация проекта

---
+++ src/main/java/com/example/Clops/ClopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Clops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClopsApplication.class, args);
+	}
+
+}
